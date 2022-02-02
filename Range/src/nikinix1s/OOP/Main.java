40d0105eaee662505a07d541d58cf0649249e2cd
Nikinix1st <98893265+nikinix1st@users.noreply.github.com>
2022-02-02 21:02:46 +0700

    Внес изменения

---
+++ Range/src/nikinix1s/OOP/Main.java
@@ -0,0 +1,14 @@
+package nikinix1s.OOP;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a = 2;
+        int d = 3;
+
+        Scanner s = new Scanner(System.in);
+
+    }
+}
